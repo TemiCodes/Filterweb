e40f37fd36fb863fbe2b63a4851a756dd14d4f51
TemiCodes <86966766+TemiCodes@users.noreply.github.com>
2023-03-06 16:34:01 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
+
 function preload(){
 
 }
 function setup(){
     canvas = createCanvas(600,400)
     canvas.center()
+    video = createCapture(VIDEO)
+    video.size(600,400)
+    stuf=ml5.posenet(video, modelLoaded())
+    stuf.on("pose",gotposes())
+}
+function modelLoaded(){
+
+}
+function gotposes(results){
+    if (results.length > 0){
+        console.log(results)
+    }
 }
